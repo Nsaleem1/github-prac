358b8435d52cc6c79d93f69623fd402bbf83c810
Nimrah <nimrahnsn10@gmail.com>
2023-06-12 08:42:57 -0700

    Initial Commit

---
+++ github prac/Second View.swift
@@ -0,0 +1,8 @@
+//
+//  Second View.swift
+//  github prac
+//
+//  Created by Nimrah Saleem on 6/12/23.
+//
+
+import Foundation
---
+++ github prac/github_pracApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_pracApp.swift
+//  github prac
+//
+//  Created by Nimrah Saleem on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct github_pracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
